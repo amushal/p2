cd450b217bb9bea44cf64dbb19dae79c3216b8b5
Ala Mushal <amushal@hotmail.com>
2018-02-20 18:15:39 -0800

    Fixed w3c validation errors: <br /> closing tag Reapplied code styling on all...

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php require 'formHandler.php'; ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,13 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Calculate product cost based on user input">
     <meta name="author" content="Ala Mushal">
-
     <title>Cost Calculator</title>
-
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
 </head>
 
 <body class="bg-light">
@@ -23,19 +19,16 @@
         <h2>Cost Calculator</h2>
         <p class="lead">Calculate the total cost and monthly payments.</p>
     </div>
-
     <div class="row">
         <div class="col-md-4 order-md-2 mb-4">
 
             <?php if ($form->isSubmitted()) : ?>
-
                 <?php if ($form->hasErrors) : ?>
 
                     <h4 class="alert alert-danger d-flex justify-content-between align-items-center mb-3">
                         <strong>Invalid input</strong>
                         <span class="badge badge-secondary badge-pill"><?= count($errors) ?></span>
                     </h4>
-
                     <ul class='list-group alert-danger'>
 
                         <?php foreach ($errors as $error) : ?>
@@ -49,7 +42,6 @@
                     <h4 class="alert alert-success d-flex justify-content-between align-items-center mb-3">
                         <strong>Result</strong>
                     </h4>
-
                     <ul class="list-group mb-3">
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
@@ -57,28 +49,24 @@
                             </div>
                             <span class="text-muted"><?= $form->get('name') ?></span>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">Email</h6>
                             </div>
                             <span class="text-muted"><?= $form->get('email') ?></span>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">Product Name</h6>
                             </div>
                             <span class="text-muted"><?= $form->get('product') ?></span>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">Price</h6>
                             </div>
                             <span class="text-muted"><?= format($price) ?></span>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">Quantity</h6>
@@ -87,6 +75,7 @@
                         </li>
 
                         <?php if ($discount != 0) : ?>
+
                             <li class="list-group-item d-flex justify-content-between bg-light">
                                 <div class="text-success">
                                     <h6 class="my-0">Discount</h6>
@@ -95,6 +84,7 @@
                                     <small><?= $discountType ?></small>
                                 </span>
                             </li>
+
                         <?php endif; ?>
 
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
@@ -105,7 +95,6 @@
                             </div>
                             <span class="text-muted"><?= format($taxRate) ?></span>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">Shipping</h6>
@@ -117,19 +106,16 @@
                             <?php endif; ?>
 
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">Number of payments</h6>
                             </div>
                             <span class="text-muted"><?= $payments ?></span>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between">
                             <span>Total (USD)</span>
                             <strong><?= format($total) ?></strong>
                         </li>
-
                         <li class="list-group-item d-flex justify-content-between">
                             <span>Monthly payments</span>
                             <strong><?= format($monthly) ?></strong>
@@ -138,9 +124,8 @@
                         <?php if ($emailMe) : ?>
                             <li class="list-group-item d-flex justify-content-between bg-light">
                                 <div class="text-success">
-                                    <h6 class="my-0">Will send the results</h6>
+                                    <h6 class="my-0">Will send the results at a later time</h6>
                                 </div>
-
                             </li>
                         <?php endif; ?>
 
@@ -150,7 +135,6 @@
             <?php endif; ?>
 
         </div>
-
 
         <div class="col-md-8 order-md-1">
 
@@ -176,7 +160,6 @@
                                    name="name"
                                    value='<?= $form->prefill('name', '') ?>'>
                         </div>
-
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="email">Email <span class="text-muted">(Optional)</span></label>
@@ -194,7 +177,6 @@
                 </div>
                 <div class="mb-3">
                     <label for="product">Product Name <span class="text-muted">(Optional)</span></label>
-
                     <input type="text"
                            class="form-control"
                            id="product"
@@ -248,8 +230,9 @@
                     </div>
                     <div class="col-md-6 mb-3 custom-control custom-checkbox">
                         <fieldset class='radios'>
-                            <label for="discountType">Discount Type </label><br/>
+                            <label for="discountType">Discount Type </label><br>
                             <label><input type='radio'
+                                          id="discountType"
                                           name='discountType'
                                           value='%' <?php if ($form->get('discountType') == '%') echo 'checked' ?>>&nbsp;Percent</label>&nbsp;&nbsp;&nbsp;
                             <label><input type='radio'
@@ -277,14 +260,12 @@
                         <label for="shipping">Shipping</label>
                         <select class="custom-select d-block w-100" id="shipping" name="shipping">
                             <option value=''>Choose one...</option>
-                            <option value='0' <?php if ($shipping == '0') echo 'selected' ?>>Free / Pickup</option>
-                            <option value='9.95' <?php if ($shipping == '9.95') echo 'selected' ?>>Standard: 1 Week $9.95</option>
-                            <option value='29.95' <?php if ($shipping == '29.95') echo 'selected' ?>>Expedite: 2nd day $29.95</option>
+                            <option value='0' <?php if ($form->get('shipping') == '0') echo 'selected' ?>>Free / Pickup</option>
+                            <option value='9.95' <?php if ($form->get('shipping') == '9.95') echo 'selected' ?>>Standard: 1 Week $9.95</option>
+                            <option value='29.95' <?php if ($form->get('shipping') == '29.95') echo 'selected' ?>>Expedite: 2nd day $29.95</option>
                         </select>
                     </div>
-
                 </div>
-
                 <div class="custom-control custom-checkbox">
                     <input type="checkbox"
                            class="custom-control-input"
@@ -293,7 +274,6 @@
                     <label class="custom-control-label" for="emailMe">Send me the results</label>
                 </div>
 
-
                 <hr class="mb-4">
 
                 <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
@@ -301,13 +281,12 @@
             </form>
 
         </div>
-
     </div>
 
     <footer class="my-5 pt-5 text-muted text-center text-small">
         <p class="mb-1">&copy; 2018 Mushal.me</p>
     </footer>
-</div>
 
+</div>
 </body>
 </html>
